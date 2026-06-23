397bb745a8038cc5c426ded241ea4047854d8156
cssibv <cssirobv@gmail.com>
2026-06-23 13:22:02 +0300

    update php

--- administrare/config.example.php
+++ administrare/config.example.php
@@ -8,4 +8,9 @@ return [
   'site_name' => 'Smart-Web Admin',
   'notify_email' => 'contact@smart-web.ro',
   'base_url'     => 'https://smart-web.ro',
+  'smtp_host' => 'mail.smart-web.ro',
+  'smtp_port' => 465,
+  'smtp_user' => 'noreply@smart-web.ro',
+  'smtp_pass' => 'PAROLA_NOREPLY',
+  'smtp_from' => 'noreply@smart-web.ro',
 ];
